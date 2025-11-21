1cee26ace34b5953809013e3e0a6230ed9fa14c5
Lilly <83720082+LOLthecool@users.noreply.github.com>
2025-11-21 15:03:30 +0000

    NOMERGE - cleanup old ui

--- scenes/player/ui/reload_avatars.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _pressed() -> void:
-	for node:Node in get_tree().get_nodes_in_group("RemoteAvatarManagers"):
-		node.change_avatar(node.current_avatar)
--- scenes/player/ui/send_message.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func send() -> void:
-	var message:String = (get_child(0) as LineEdit).text.strip_escapes()
-	if message.is_empty():
-		return
-	(get_child(0) as LineEdit).clear()
-	GlobalWorldHandler.current_world.chat_box_manager.send_message(message)
