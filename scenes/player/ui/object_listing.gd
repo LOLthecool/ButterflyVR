7ae3d73f4b514e3b83145f7afa58ba4b20058694
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-01-13 19:16:01 +0000

    avatar page done

--- scenes/player/ui/object_listing.gd
+++ scenes/player/ui/object_listing.gd
@@ -9,6 +9,10 @@ func create(object:Dictionary[String, Variant], object_type:LRUCache.ObjectType)
 	self.object = object
 	text = object["name"]
 	icon = ImageTexture.create_from_image(await GlobalImageDownloadHandler.get_object(UUID.from_String(object["id"]), object_type))
+	icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	vertical_icon_alignment = VERTICAL_ALIGNMENT_TOP
+	expand_icon = true
+	custom_minimum_size = Vector2(192, 192)
 
 func _pressed() -> void:
 	object_selected.emit(object)
--- scenes/player/ui/previewer.gd
+++ scenes/player/ui/previewer.gd
@@ -3,16 +3,23 @@ extends Node3D
 class_name Previewer
 
 @export var viewport:SubViewport
+@export var camera:Camera3D
 
 func create_preview(uuid:UUID) -> void:
-	get_child(0).queue_free()
+	if get_child_count() != 0:
+		get_child(0).queue_free()
 	
 	rotation.y = 0
 	
 	var avatar_scene:PackedScene = await GlobalDownloadHandler.get_object(
 			uuid, LRUCache.ObjectType.avatar)
 	
+	if !avatar_scene:
+		push_error("failed to load avatar %s for preview" % uuid)
+		return
+	
 	if !SetupHelpers.check_safe(avatar_scene.get_state()):
+		push_error("tried to preview unsafe avatar!")
 		return
 	
 	var avatar:Node = avatar_scene.instantiate()
@@ -22,7 +29,8 @@ func create_preview(uuid:UUID) -> void:
 	var aabb:AABB = AABB()
 	for child:Node in SetupHelpers.get_node_and_children_recursive(avatar):
 		if child is VisualInstance3D:
-			aabb.merge((child as VisualInstance3D).get_aabb())
+			(child as VisualInstance3D).layers = 4096 # layer 13
+			aabb = aabb.merge((child as VisualInstance3D).get_aabb())
 	var pos:Vector3 = aabb.position + (aabb.size / 2)
 	# todo: something is broken here.
 	# this should place the camera as close as possible to the object,
@@ -32,6 +40,8 @@ func create_preview(uuid:UUID) -> void:
 	# with fov / 2 being the angle and aabb.size.y / 2 being the opposite side
 	# solving for the adjacent side should give us the camera distance but it dosent
 	pos.z += (maxf(aabb.size.x, aabb.size.y) / 2) / abs(tan(FOV / 2)) * 1.1
+	
+	camera.position = pos
 
 func _process(delta: float) -> void:
 	rotation.y += 0.2 * delta
