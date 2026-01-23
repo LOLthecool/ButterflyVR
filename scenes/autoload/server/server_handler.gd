744a87a55f444d4213201ffbc41e196eeb9f4be4
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-01-23 14:41:24 +0000

    started gameserver work

--- scenes/autoload/server/server_handler.gd
+++ scenes/autoload/server/server_handler.gd
@@ -1,12 +1,16 @@
 extends Node
 class_name ServerHandler
 
+const INSTANCE_QUEUE_ENDPOINT = "/api/v0/server/instance"
+
 const LOCAL_SERVER_KEY_LOCATION:String = "user://local_key.tmp"
+
 # seconds after all players leave before exiting
 const INACTIVITY_KILL_THRESHOLD:float = 5.0
 
 var started:bool = false
 var finished_starting:bool = false
+var agones_health_checking:bool = false
 var api_token:PackedByteArray
 var max_players:int
 var inactivity:float
@@ -16,32 +20,59 @@ func _ready() -> void:
 	if !started:
 		queue_free()
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if agones_health_checking:
+		AgonesSDK.health()
 	if finished_starting and NetworkManager.get_player_count() == 0:
 		inactivity += delta
 		if inactivity > INACTIVITY_KILL_THRESHOLD:
 			push_warning("too long with 0 players: exiting")
-			get_tree().quit()
+			if agones_health_checking:
+				AgonesSDK.shutdown()
+			else:
+				get_tree().quit()
 	else:
 		inactivity = 0
 
-# this should do nothing until this function is done
+# this class should do nothing until this function is done
 func start(max_players:int, api_token:PackedByteArray, is_local:bool, 
 		world:UUID, bind_addr:String, key:PackedByteArray) -> void:
 	started = true
 	
-	await GlobalAccountHandler.set_token(api_token, -1, false)
-	
-	print("loading world: %s" % world)
-	await GlobalWorldHandler.load_world_server(world, bind_addr, key)
-	
 	if is_local:
 		# local instance
+		
+		await GlobalAccountHandler.set_token(api_token, -1, false)
+		
+		await GlobalWorldHandler.load_world_server(world, bind_addr, key)
+		
 		var local_token_file:FileAccess = FileAccess.open(LOCAL_SERVER_KEY_LOCATION, FileAccess.WRITE)
 		local_token_file.store_buffer(NetworkManager.get_next_client())
-		local_token_file.flush()
 		local_token_file.close()
 	else:
 		# game server instance
-		pass # todo: verify instance token, open socket and register client tokens with api
+		AgonesSDK.start()
+		agones_health_checking = true
+		
+		while true:
+			AgonesSDK.reserve(3)
+			var response:Array[Variant] = await GlobalAPIHandler.make_request(HTTPClient.METHOD_GET, INSTANCE_QUEUE_ENDPOINT, PackedStringArray([GlobalAccountHandler.get_token_header()]))
+			var result:Array[Variant] = GlobalAPIHandler.handle_response(response[0], response[2], [200], ["uuid", "world_uuid", "token"])
+			
+			var success:bool = result[0]
+			var values:Dictionary[String, Variant] = {}
+			values.assign(result[4])
+			
+			if !success:
+				await get_tree().create_timer(6).timeout
+				continue
+			
+			AgonesSDK.allocate()
+			
+			await GlobalAccountHandler.set_token(values["token"], -1, false)
+			
+			print("loading world: %s" % values["world_uuid"])
+			await GlobalWorldHandler.load_world_server(values["world_uuid"], bind_addr, key)
+			break
+	
 	finished_starting = true
