dcadf2d8a5e01dee939247bf9a7c1d6523db5f11
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-07-07 15:51:25 +0100

    zstd better error handling

--- rust/godot-rust-zstd/src/lib.rs
+++ rust/godot-rust-zstd/src/lib.rs
@@ -20,63 +20,73 @@ struct ZSTDCompressor {
 impl ZSTDCompressor {
     #[func]
     fn compress_file_to_file(&mut self, input_path: String, output_path: String) -> bool {
-        let Ok(mut input) = File::open(&input_path) else {
-            godot_error!("error while opening input file");
+        let Ok(mut input) = File::open(&input_path)
+            .inspect_err(|e| godot_error!("error while opening input file: {}", e))
+        else {
             return false;
         };
-        let Ok(mut output) = File::create(&output_path) else {
-            godot_error!("error while creating output file");
+        let Ok(mut output) = File::create(&output_path)
+            .inspect_err(|e| godot_error!("error while creating output file: {}", e))
+        else {
             return false;
         };
-        let Ok(mut encoder) = Encoder::new(&mut output, 3) else {
-            godot_error!("error while creating encoder");
+        let Ok(mut encoder) = Encoder::new(&mut output, 3)
+            .inspect_err(|e| godot_error!("error while creating encoder: {}", e))
+        else {
             return false;
         };
         let mut buf = vec![0u8; CHUNK_SIZE];
         loop {
-            let Ok(bytes_read) = input.read(&mut buf) else {
-                godot_error!("error while reading input");
+            let Ok(bytes_read) = input
+                .read(&mut buf)
+                .inspect_err(|e| godot_error!("error while reading input: {}", e))
+            else {
                 return false;
             };
             if bytes_read == 0 {
                 break;
             }
-            if encoder.write_all(&buf[..bytes_read]).is_err() {
-                godot_error!("error while writing to output");
+            if let Err(e) = encoder.write_all(&buf[..bytes_read]) {
+                godot_error!("error while writing to output: {}", e);
                 return false;
             }
         }
-        if encoder.finish().is_err() {
-            godot_error!("error while finishing encoder");
+        if let Err(e) = encoder.finish() {
+            godot_error!("error while finishing encoder: {}", e);
             return false;
         }
         true
     }
     #[func]
     fn decompress_file_to_file(&mut self, input_path: String, output_path: String) -> bool {
-        let Ok(mut input) = File::open(&input_path) else {
-            godot_error!("error while opening input file");
+        let Ok(mut input) = File::open(&input_path)
+            .inspect_err(|e| godot_error!("error while opening input file: {}", e))
+        else {
             return false;
         };
-        let Ok(mut output) = File::create(&output_path) else {
-            godot_error!("error while creating output file");
+        let Ok(mut output) = File::create(&output_path)
+            .inspect_err(|e| godot_error!("error while creating output file: {}", e))
+        else {
             return false;
         };
-        let Ok(mut decoder) = Decoder::new(&mut input) else {
-            godot_error!("error while creating encoder");
+        let Ok(mut decoder) = Decoder::new(&mut input)
+            .inspect_err(|e| godot_error!("error while creating decoder: {}", e))
+        else {
             return false;
         };
         let mut buf = vec![0u8; CHUNK_SIZE];
         loop {
-            let Ok(bytes_read) = decoder.read(&mut buf) else {
-                godot_error!("error while reading input");
+            let Ok(bytes_read) = decoder
+                .read(&mut buf)
+                .inspect_err(|e| godot_error!("error while reading input: {}", e))
+            else {
                 return false;
             };
             if bytes_read == 0 {
                 break;
             }
-            if output.write_all(&buf[..bytes_read]).is_err() {
-                godot_error!("error while writing to output");
+            if let Err(e) = output.write_all(&buf[..bytes_read]) {
+                godot_error!("error while writing to output: {}", e);
                 return false;
             }
         }
