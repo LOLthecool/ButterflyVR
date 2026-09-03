088262b923126b8d33309e2fe7b518e9d257d5d5
Lilly <83720082+LOLthecool@users.noreply.github.com>
2026-09-03 09:39:51 +0100

    finally managed to start working again

--- rust/pck_checker/src/lib.rs
+++ rust/pck_checker/src/lib.rs
@@ -14,19 +14,39 @@ struct MyExtension;
 unsafe impl ExtensionLibrary for MyExtension {}
 
 /// SAFETY: Implementers must have no invalid bit patterns. Or more explicitly:
-/// For every possible byte sequence of length size_of::<impl DirectlyReadable>(),
-/// that sequence constitutes a valid representation of the type.
-// yes technically a type with an empty Drop impl or a union with an empty tuple can be DirectlyReadable
-// but not Copy but its better to reject those edge cases to avoid the footgun here.
+/// For every possible byte sequence of length size_of::<T>(),
+/// that sequence is a valid representation of T.
+// yes technically a type with an empty Drop impl can be DirectlyReadable but not Copy.
 unsafe trait DirectlyReadable: Copy {}
 
 unsafe impl DirectlyReadable for u8 {}
 unsafe impl DirectlyReadable for u32 {}
 unsafe impl DirectlyReadable for u64 {}
 
-const VERSION: [u32; 3] = [3, 4, 6];
+const VERSION: &[u32] = &[3, 4, 6];
 const BAD_EXTENSIONS: &[&str] = &[".res", ".tres", ".gd", ".gdc", ".remap", ".import"];
 const UNSUPPORTED_EXTENSIONS: &[&str] = &[".scn", ".escn"];
+
+macro_rules! unwrap_or_return {
+    ($expr:expr, $default:expr, Result) => {
+        match $expr {
+            Ok(x) => x,
+            Err(e) => {
+                godot_warn!("Error at line {}: {}", e, line!());
+                return $default;
+            }
+        }
+    };
+    ($expr:expr, $default:expr, Option) => {
+        match $expr {
+            Some(x) => x,
+            None => {
+                godot_warn!("Error at line {}: unexpected None", line!());
+                return $default;
+            }
+        }
+    };
+}
 
 #[derive(GodotClass)]
 #[class(init, singleton)]
@@ -43,7 +63,7 @@ impl PCKChecker {
         object_type_string: String,
         uuid_string: String,
     ) -> bool {
-        let mut pck = BufReader::new(File::open(pck_path).unwrap());
+        let mut pck = BufReader::new(unwrap_or_return!(File::open(pck_path), false, Result));
 
         // no offset support, shouldnt be an issue
         // if some people are failing this check it might be the endianness issue in read_value
@@ -55,18 +75,19 @@ impl PCKChecker {
 
         for v in VERSION {
             let version: u32 = read_value(&mut pck);
-            if version != v {
+            if version != *v {
                 godot_error!("version mismatch: got {}, expected {}", version, v);
                 return false;
             }
         }
+
         // ignore patch version and flag8
-        pck.seek_relative(8).unwrap();
+        unwrap_or_return!(pck.seek_relative(8), false, Result);
 
         let file_base: u64 = read_value(&mut pck);
         let dir_offset: u64 = read_value(&mut pck);
 
-        pck.seek(SeekFrom::Start(dir_offset)).unwrap();
+        unwrap_or_return!(pck.seek(SeekFrom::Start(dir_offset)), false, Result);
 
         let file_count: u32 = read_value(&mut pck);
 
@@ -75,16 +96,19 @@ impl PCKChecker {
         for _ in 0..file_count {
             let length_hint: u32 = read_value(&mut pck);
             let mut bytes = vec![0; length_hint as usize];
-            pck.read_exact(&mut bytes).unwrap();
-            let path = String::from_utf8(bytes).unwrap();
+            unwrap_or_return!(pck.read_exact(&mut bytes), false, Result);
+            let path = unwrap_or_return!(String::from_utf8(bytes), false, Result);
 
-            let path = String::from_utf8(
-                path.into_bytes()
-                    .into_iter()
-                    .take_while(|x| *x != 0)
-                    .collect(),
-            )
-            .unwrap();
+            let path = unwrap_or_return!(
+                String::from_utf8(
+                    path.into_bytes()
+                        .into_iter()
+                        .take_while(|x| *x != 0)
+                        .collect(),
+                ),
+                false,
+                Result
+            );
 
             // contains("..") check appears unneeded currently, can be skipped if it causes issues
             if path.contains("..")
@@ -115,8 +139,10 @@ impl PCKChecker {
 
             let offset: u64 = read_value(&mut pck);
             let size: u64 = read_value(&mut pck);
+
             // skip md5 hash and flags
-            pck.seek_relative(20).unwrap();
+            unwrap_or_return!(pck.seek_relative(20), false, Result);
+
             if path.ends_with(".tscn") {
                 tscn_files.push(PCKFile {
                     path,
@@ -136,25 +162,147 @@ impl PCKChecker {
     }
 
     fn check_tscn(&self, file: &PCKFile, pck: &mut BufReader<File>) -> bool {
-        pck.seek(SeekFrom::Start(file.absolute_offset)).unwrap();
+        unwrap_or_return!(
+            pck.seek(SeekFrom::Start(file.absolute_offset)),
+            false,
+            Result
+        );
 
         let mut buffer: Vec<u8> = vec![0; 19];
-        pck.read_exact(&mut buffer).unwrap();
-        let buffer = String::from_utf8(buffer).unwrap();
+        unwrap_or_return!(pck.read_exact(&mut buffer), false, Result);
+        let buffer = unwrap_or_return!(String::from_utf8(buffer), false, Result);
         if buffer != "[gd_scene format=4]" && buffer != "[gd_scene format=3]" {
             godot_error!("invalid header in scene {:?}. got {:?}", file.path, buffer);
             return false;
         }
 
-        while pck.stream_position().unwrap() < file.absolute_offset + file.size {
+        while unwrap_or_return!(pck.stream_position(), false, Result)
+            < file.absolute_offset + file.size
+        {
             let next: u8 = read_value(pck);
+
             if next <= 32 {
                 continue;
+            }
+
+            if next != '[' as u8 {
+                godot_error!(
+                    "invalid character {:?} in scene {:?}, expected '['",
+                    next as char,
+                    file.path
+                );
+                return false;
+            }
+
+            match parse_header(pck) {
+                Ok(object_type) => {
+                    if let Err(e) = parse_object(pck, object_type) {
+                        godot_error!("object parse error in scene {:?}: {:?}", file.path, e);
+                        return false;
+                    }
+                    continue;
+                }
+                Err(e) => {
+                    godot_error!("header parse error in scene {:?}: {:?}", file.path, e);
+                    return false;
+                }
             }
         }
 
         return true;
     }
+}
+
+#[derive(Debug)]
+enum ObjectType {
+    Node,
+    SubResource,
+}
+
+#[derive(Debug)]
+enum HeaderParseError {
+    ConnectionType,
+    ExtResourceType,
+    UnknownResourceType,
+    GenericError,
+}
+
+#[derive(Debug)]
+enum ObjectParseError {
+    BadKey,
+    BadValue,
+    InlineOjectValue,
+    BadResource,
+    GenericError,
+}
+
+fn parse_header(pck: &mut BufReader<File>) -> Result<ObjectType, HeaderParseError> {
+    let mut type_buffer: Vec<u8> = Vec::with_capacity(16);
+    unwrap_or_return!(
+        pck.read_until(' ' as u8, &mut type_buffer),
+        Err(HeaderParseError::GenericError),
+        Result
+    );
+    let type_buffer = unwrap_or_return!(
+        str::from_utf8(&type_buffer),
+        Err(HeaderParseError::GenericError),
+        Result
+    );
+
+    match type_buffer {
+        "sub_resource " => Ok(ObjectType::SubResource),
+        "node " => Ok(ObjectType::Node),
+        "ext_resource " => Err(HeaderParseError::ExtResourceType),
+        "connection " => Err(HeaderParseError::ConnectionType),
+        _ => Err(HeaderParseError::UnknownResourceType),
+    }
+}
+
+fn parse_object(
+    pck: &mut BufReader<File>,
+    object_type: ObjectType,
+) -> Result<(), ObjectParseError> {
+    match object_type {
+        ObjectType::Node => parse_node(pck),
+        ObjectType::SubResource => {
+            let mut buffer: Vec<u8> = Vec::with_capacity(6);
+            unwrap_or_return!(
+                pck.read_until('"' as u8, &mut buffer),
+                Err(ObjectParseError::GenericError),
+                Result
+            );
+            let buffer = unwrap_or_return!(
+                String::from_utf8(buffer),
+                Err(ObjectParseError::GenericError),
+                Result
+            );
+
+            if buffer != "type=\"" {
+                return Err(ObjectParseError::GenericError);
+            }
+
+            let mut buffer: Vec<u8> = Vec::new();
+            unwrap_or_return!(
+                pck.read_until('"' as u8, &mut buffer),
+                Err(ObjectParseError::GenericError),
+                Result
+            );
+            let buffer = unwrap_or_return!(
+                String::from_utf8(buffer),
+                Err(ObjectParseError::GenericError),
+                Result
+            );
+            let buffer = &buffer[..buffer.len() - 1];
+            if !(resources::ResourceWhitelist::iter().any(|f| f.to_string() == buffer)) {
+                return Err(ObjectParseError::BadResource);
+            }
+            Ok(())
+        }
+    }
+}
+
+fn parse_node(pck: &mut BufReader<File>) -> Result<(), ObjectParseError> {
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -172,7 +320,8 @@ fn read_value<T: DirectlyReadable>(file: &mut impl Read) -> T {
 
         let bytes = slice::from_raw_parts_mut(&raw mut value as *mut u8, std::mem::size_of::<T>());
 
-        file.read_exact(bytes).unwrap();
+        // partial write is not an issue since we will never be invalid
+        unwrap_or_return!(file.read_exact(bytes), value, Result);
 
         value
     }
